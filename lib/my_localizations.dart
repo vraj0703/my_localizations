ebc9628a8deac9d269d5de19c000ade8192ce68c
vraj0703 <vraj0703@gmail.com>
2025-11-23 19:33:29 +0530

    add allowed locale

--- lib/my_localizations.dart
+++ lib/my_localizations.dart
@@ -1,8 +1,15 @@
-
 import 'my_localizations_platform_interface.dart';
 
 class MyLocalizations {
+  static late List<String> _allowedLocale;
+
+  static Future<void> initialize({required List<String> allowedLocale}) async {
+    _allowedLocale = allowedLocale;
+  }
+
   Future<String?> getPlatformVersion() {
     return MyLocalizationsPlatform.instance.getPlatformVersion();
   }
+
+  static List<String> get allowedLocale => _allowedLocale;
 }
