15c1592c23f4fdc56772aa6c35b35a34e6f3e74c
vraj0703 <vraj0703@gmail.com>
2025-11-25 12:35:43 +0530

    $string

--- lib/library.dart
+++ lib/library.dart
@@ -1,5 +1,9 @@
 library;
 
+import 'package:my_localizations/l10n/app_localizations.dart';
+
 export 'package:my_localizations/my_localizations.dart';
 export 'package:my_localizations/language_model.dart';
 export 'package:my_localizations/l10n/app_localizations.dart';
+
+typedef $strings = AppLocalizations;
